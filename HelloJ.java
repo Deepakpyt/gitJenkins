4be3a594248366d0727a1beedb30d5db045c9724
Deepakpyt <erdeepak.016@gmail.com>
2023-05-08 18:30:43 +0530

    tt

--- HelloJ.java
+++ HelloJ.java
@@ -1,16 +1,8 @@
 class HelloJ{
     public static void main(String[] args){
         System.out.println("Hello Jenkins.");
-<<<<<<< HEAD
-<<<<<<< HEAD
         System.out.println("I Updated this code on github...");
         System.out.println("I pushed changes on git hub.");
-=======
-=======
->>>>>>> e0e0cbf8ed2666be97e125c16df0d66a794da2ae
-        System.out.println("I Updated this code on github...");
-        // System.out.println("I Updated this code again");
->>>>>>> 9a453ea0af1b52e28803939b07e52a37456d470b
 
     }
 }
