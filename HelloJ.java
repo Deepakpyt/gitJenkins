39e8b189bde4c4fcdfec273fb09c6d814feaaaaf
Deepakpyt <erdeepak.016@gmail.com>
2023-05-08 18:08:15 +0530

    something

--- HelloJ.java
+++ HelloJ.java
@@ -2,7 +2,7 @@ class HelloJ{
     public static void main(String[] args){
         System.out.println("Hello Jenkins.");
         System.out.println("I Updated this code on github");
-        // System.out.println("I Updated this code again");
+        System.out.println("I pushed changes on git hub.");
 
     }
 }
