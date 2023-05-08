19b3d9c8572395c23b5f2706e0ba661a5e310492
Deepakpyt <erdeepak.016@gmail.com>
2023-05-08 17:48:36 +0530

    My code

---
+++ HelloJ.java
@@ -0,0 +1,8 @@
+class HelloJ{
+    public static void main(String[] args){
+        System.out.println("Hello Jenkins.");
+        // System.out.println("I Updated this code");
+        // System.out.println("I Updated this code again");
+
+    }
+}
